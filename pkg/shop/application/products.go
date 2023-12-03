e2da2231c4688793f2e9972ab66e9e18dc125dbc
Kapil Ramnani <kramnani95@gmail.com>
2023-12-03 11:36:46 +0530

    product repo + price and product classes/blueprint + product application

--- pkg/shop/application/products.go
+++ pkg/shop/application/products.go
@@ -1,5 +1,12 @@
 package application
 
+import (
+	"errors"
+
+	"github.com/kaps1195/go-mono-to-micro/pkg/common/price"
+	"github.com/kaps1195/go-mono-to-micro/pkg/shop/domain/products"
+)
+
 type productReadModel interface {
 	AllProducts() ([]products.Product, err)
 }
@@ -7,6 +14,14 @@ type productReadModel interface {
 type ProductsService struct {
 	repo      products.Repository
 	readModel productReadModel
+}
+
+type AddProductCommand struct {
+	ID            string
+	Name          string
+	Description   string
+	Pricecents    uint
+	PriceCurrency string
 }
 
 func NewProductsService() ProductsService {
@@ -17,6 +32,21 @@ func (s ProductsService) AllProducts() {
 
 }
 
-func (s ProductsService) AddProduct() error {
+func (s ProductsService) AddProduct(cmd AddProductCommand) error {
 
+	price, err := price.NewPrice(cmd.Pricecents, cmd.PriceCurrency)
+	if err != nil {
+		return errors.Wrap(err, "Invalid product price")
+	}
+
+	p, err := products.NewProduct(products.ID(cmd.ID), cmd.Name, cmd.Description, price)
+	if err != nil {
+		return errors.Wrap(err, "Cannot create product")
+	}
+
+	if err := s.repo.Save(p); err != nil {
+		return errors.Wrap(err, "Cannot save product")
+	}
+
+	return nil
 }
